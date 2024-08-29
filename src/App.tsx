ec3cd114aa259c3e2ec330b1db2cb9da6bf94b93
Yanis Sahnoune <yanissahnoune@MBP-de-Yanis-2.lan>
2024-08-29 08:43:23 +0200

    feat/add-tabs: add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import { Header } from './components/organisms/Header/Header'
 function App() {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Navigation />
+      <div className='flex flex-col min-h-screen'>
+        <Header />
+        <div className='flex-grow flex items-center justify-center py-16'>
+          <Outlet />
+        </div>
+        <Navigation />
+      </div>
     </>
   )
 }
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -2,7 +2,7 @@ import logoWinamax from '/logo.png'
 
 export const Header = () => {
     return <>
-    <header className='flex justify-center border-b-[1.5px] border-[#c2c7d6] mb-2'>
+    <header className='z-20 fixed w-full bg-white flex justify-center border-b-[1.5px] border-[#c2c7d6] mb-2'>
         <img src={logoWinamax} width={50}/>
     </header>
     </>
