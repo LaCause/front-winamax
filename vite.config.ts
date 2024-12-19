70dc8dea2d4ad757def986832f9e2465189d8163
LaCause <yanis.sahnoune.dev@gmail.com>
2024-12-19 23:39:41 +0100

    fix/vite-config: fix deployment vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/front-winamax/',
-  build: {
-    outDir: 'dist',
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/front-winamax/' : '/',
+    build: {
+      outDir: 'dist',
+    },
+  };
 });
